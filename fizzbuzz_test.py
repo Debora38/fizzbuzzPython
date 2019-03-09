c562cae33d3bb09f28e0720c497dc7cbd65cee6b
debora <debora.piu92@gmail.com>
2019-03-09 21:10:36 +0000

    more numbers in tests

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,12 +5,24 @@ class MyFirstTests(unittest.TestCase):
 
     def test_number(self):
         self.assertEqual(check_fizzbuzz(1), 1)
+        self.assertEqual(check_fizzbuzz(34), 34)
+        self.assertEqual(check_fizzbuzz(67), 67)
 
     def test_fizz(self):
         self.assertEqual(check_fizzbuzz(3), 'fizz')
+        self.assertEqual(check_fizzbuzz(9), 'fizz')
+        self.assertEqual(check_fizzbuzz(36), 'fizz')
+        self.assertEqual(check_fizzbuzz(66), 'fizz')
 
     def test_buzz(self):
         self.assertEqual(check_fizzbuzz(5), 'buzz')
+        self.assertEqual(check_fizzbuzz(50), 'buzz')
+        self.assertEqual(check_fizzbuzz(65), 'buzz')
+
+    def test_check_fizzbuzz(self):
+        self.assertEqual(check_fizzbuzz(15), 'fizzbuzz')
+        self.assertEqual(check_fizzbuzz(75), 'fizzbuzz')
+        self.assertEqual(check_fizzbuzz(45), 'fizzbuzz')
 
     def test_fizzbuzz(self):
-        self.assertEqual(check_fizzbuzz(15), 'fizzbuzz')
+        self.assertEqual(fizzbuzz(), '')
